fc6668d481aa4a207c813fc8c6c0ec6e8d486982
mikboy018 <boyer.mike.e@gmail.com>
2017-12-26 21:41:01 -0500

    Basic JS file added
    Prompts, Alerts, & Console commands

---
+++ Class/script.js
@@ -0,0 +1,8 @@
+//alert("hello from the JS file")
+var firstName = prompt("What is your first name?");
+//alert("Nice to meet you " + userName);
+var lastName = prompt("What is your last name?")
+var age = prompt("What is your age?");
+//console.log("I see your name is: " + userName);
+alert("Your full name is: " + firstName + " " + lastName + ".");
+alert("You are " + age + " years old.");
